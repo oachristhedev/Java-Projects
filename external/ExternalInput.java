5a36f6e5692bb1c85573f5c14e20d1959e57dff3
oachristhedev <oachristhedev.@gmail.com>
2026-06-07 21:27:37 +0000

    shared model

--- external/ExternalInput.java
+++ external/ExternalInput.java
@@ -7,41 +7,41 @@ public class ExternalInput {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Surname: ");
-        String surname = input.nextLine();
-
-        System.out.println("First Names: ");
-        String firstNames = input.nextLine();
-
-        System.out.println("Student ID: ");
-        String studentId = input.nextLine();
-
-        System.out.println("Course Name: ");
-        String courseName = input.nextLine();
-
-        System.out.println("Class: ");
-        char className = input.nextLine().charAt(0);
-
-        System.out.println("Level: ");
-        int level = input.nextInt();
-        input.nextLine();
-
-        System.out.println("Phone Number: ");
-        String phone = input.nextLine();
-
-        Student student = new Student(
-                surname,
-                firstNames,
-                studentId,
-                courseName,
-                className,
-                level,
-                phone
-        );
-
-        System.out.println();
-        student.display();
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Surname: ");
+            String surname = input.nextLine();
+            
+            System.out.println("First Names: ");
+            String firstNames = input.nextLine();
+            
+            System.out.println("Student ID: ");
+            String studentId = input.nextLine();
+            
+            System.out.println("Course Name: ");
+            String courseName = input.nextLine();
+            
+            System.out.println("Class: ");
+            char className = input.nextLine().charAt(0);
+            
+            System.out.println("Level: ");
+            int level = input.nextInt();
+            input.nextLine();
+            
+            System.out.println("Phone Number: ");
+            String phone = input.nextLine();
+            
+            Student student = new Student(
+                    surname,
+                    firstNames,
+                    studentId,
+                    courseName,
+                    className,
+                    level,
+                    phone
+            );
+            
+            System.out.println();
+            student.display();
+        }
     }
 }
